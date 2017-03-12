488e238dd7c47d5fc23dde556f04b8f1ef351bbe
Jason Goldfine-Middleton <jrgoldfinemiddleton@gmail.com>
2017-03-12 12:49:58 -0700

    Add skeleton code to unit test method

--- URLValidator/src/UrlValidatorTest.java
+++ URLValidator/src/UrlValidatorTest.java
@@ -89,9 +89,102 @@ public class UrlValidatorTest extends TestCase {
    
    public void testIsValid()
    {
-	   for(int i = 0;i<10000;i++)
-	   {
-		   
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   
+	   final String[] validSchemes = {
+			   
+	   };
+	   
+	   final String[] validAuthorities = {
+			   
+	   };
+	   
+	   final String[] validPorts = {
+			   
+	   };
+	   
+	   final String[] validPaths = {
+			   
+	   };
+	   
+	   final String[] validQueries = {
+			   
+	   };
+	   
+	   final String[] invalidSchemes = {
+			   
+	   };
+	   
+	   final String[] invalidAuthorities = {
+			   
+	   };
+	   
+	   final String[] invalidPorts = {
+			   
+	   };
+	   
+	   final String[] invalidPaths = {
+			   
+	   };
+	   
+	   final String[] invalidQueries = {
+			   
+	   };	   
+	   
+	   final String[][] schemeSets = {
+			   validSchemes,
+			   invalidSchemes
+	   };
+	   
+	   final String[][] authoritySets = {
+			   validAuthorities,
+			   invalidAuthorities
+	   };
+	   
+	   final String[][] portSets = {
+			   validPorts,
+			   invalidPorts
+	   };
+	   
+	   final String[][] pathSets = {
+			   validPaths,
+			   invalidPaths
+	   };
+	   
+	   final String[][] querySets = {
+			   validQueries,
+			   invalidQueries
+	   };
+	   
+	   for (int p1 = 0; p1 != 2; ++p1) {
+		   for (int q1 = 0; q1 != schemeSets[p1].length; ++q1) {
+			   for (int p2 = 0; p2 != 2; ++p2) {
+				   for (int q2 = 0; q2 != authoritySets[p2].length; ++q2) {
+					   for (int p3 = 0; p3 != 2; ++p3) {
+						   for (int q3 = 0; q3 != portSets[p3].length; ++q3) {
+							   for (int p4 = 0; p4 != 2; ++p4) {
+								   for (int q4 = 0; q4 != pathSets[p4].length; ++q4) {
+									   for (int p5 = 0; p5 != 2; ++p5) {
+										   for (int q5 = 0; q5 != querySets[p5].length; ++q5) {
+											   String testUrl = schemeSets[p1][q1]
+													   + authoritySets[p2][q2]
+													   + portSets[p3][q3]
+													   + pathSets[p4][q4]
+													   + querySets[p5][q5];
+											   boolean expected = (q1 & q2 & q3 & q4 & q5) == 1;
+											   boolean actual = urlVal.isValid(testUrl);
+											   if (expected != actual) {
+												   System.out.println(testUrl + ": " + actual);
+											   }
+										   }
+									   }
+								   }
+							   }
+						   }
+					   }
+				   }
+			   }
+		   }
 	   }
    }
    
